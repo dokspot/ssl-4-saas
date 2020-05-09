20a66c42090c0d942d90829938b42e95a90b947f
Cyrille <c.derche@me.com>
2020-05-09 23:30:08 +0200

    imp account setup

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -15,7 +15,7 @@ class Tenant < ApplicationRecord
   validates :terms_of_service_agreed, presence: true
   validates :terms_of_service_agreed, acceptance: true
 
-  before_create :create_lets_encrypt_account
+  before_create :account_setup
 
   private
 
@@ -29,10 +29,14 @@ class Tenant < ApplicationRecord
     self.private_key = OpenSSL::PKey::RSA.new(4096)
   end
 
-  def create_lets_encrypt_account
-    gen_private_key if private_key.nil?
+  def gen_kid
     client = Acme::Client.new(private_key: OpenSSL::PKey::RSA.new(private_key), directory: ENV["LE_DIRECTORY"])
     account = client.new_account(contact: "mailto:#{email}", terms_of_service_agreed: terms_of_service_agreed)
     self.kid = account.kid
+  end
+
+  def account_setup
+    gen_private_key
+    gen_kid
   end
 end
